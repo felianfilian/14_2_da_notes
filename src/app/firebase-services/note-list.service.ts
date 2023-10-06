7438e98aa574dd93b88e5d652050d428752fc89b
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-10-06 21:20:26 +0200

    database fetch

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collection, doc } from '@angular/fire/firestore';
+import { Note } from '../interfaces/note.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NoteListService {
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
 
-  constructor() { }
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {}
+
+  // const itemCollection = collection(this.firestore, 'items');
+
+  getNoteRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
 }
